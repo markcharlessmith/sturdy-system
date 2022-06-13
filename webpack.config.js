7f7c48415dfd27458266adf2e10506b0bd9b0310
Mark Charles Smith <markcharlessmith@gmail.com>
2022-06-13 10:56:34 -0700

    added boilerplate for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+/* eslint-disable no-unused-vars */
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: [
+    // entry point of our app
+    './client/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devtool: 'eval-source-map',
+  mode: process.env.NODE_ENV,
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    // match the output path
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      // match the output 'publicPath'
+      publicPath: '/',
+    },
+    // enable HMR on the devServer
+    hot: true,
+    // match the output 'publicPath'
+    // publicPath: '/',
+    // fallback to root for other urls
+    // historyApiFallback: true,
+
+    // headers: { 'Access-Control-Allow-Origin': '*' },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    },
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+      // '/': {
+      //   target: 'http://localhost:3000/',
+      //   secure: false,
+      // },
+      '/assets/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+      '/github': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+      '/logout': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s?[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
+};
